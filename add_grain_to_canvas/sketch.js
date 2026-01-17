1e5fa6614766e280e27d0c523b41570a35d05714
megamitts <megamitts@gmail.com>
2026-01-17 08:00:31 +0000

    Add grain effect to canvas drawing

---
+++ add_grain_to_canvas/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  fill('yellow');
+  circle(200,200,200);
+  addGrain();
+}
+
+function addGrain() {
+  loadPixels();
+  const grainAmount = 64;
+  for (let i = 0; i < pixels.length; i += 4) {
+    const n = random(-grainAmount, grainAmount);
+    pixels[i] += n;     // R
+    pixels[i + 1] += n; // G
+    pixels[i + 2] += n; // B
+    pixels[i + 3] += n; // Alpha
+  }
+  updatePixels();
+}
