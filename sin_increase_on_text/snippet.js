6b079c642f05217e1f0a188127ea6d74a5da72d6
megamitts <megamitts@gmail.com>
2026-01-27 19:24:59 +0000

    Create snippet.js

---
+++ sin_increase_on_text/snippet.js
@@ -0,0 +1,12 @@
+let minVal = 1;
+  let maxVal = 40;
+  let calculatedValue = minVal + ((sin(t) + 1) / 2) * (maxVal - minVal);
+  
+  textSize(calculatedValue);
+
+/* This snippet will lock the sin range to the minVal and maxVal
+
+
+t is the angle increased usually by 0.02 to look smooth
+
+*/
